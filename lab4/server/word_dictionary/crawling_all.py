559956f5c0c23531d1b5f8569063d9ad7576e328
woonsan <doldol9080@kookmin.ac.kr>
2023-08-18 06:28:15 +0900

    여러 페이지 크롤링 가능하게 변경

---
+++ lab4/server/word_dictionary/crawling_all.py
@@ -0,0 +1,203 @@
+import requests
+from bs4 import BeautifulSoup
+from kiwipiepy import Kiwi
+import time
+def get_search_results(search_query):
+    nownum = 2000
+    search_url = "https://ko.wikipedia.org/w/index.php?search=" + search_query + "&title=특수:검색&profile=default&fulltext=1&ns0=1&offset=0&limit=2000"
+    # limit이 몇 개씩 단어 보여주는지 알려주는거 offset이 시작번호 0으로 하면 처음부터 
+    # 먼저 2000개 정도 찾고 총 개수 알아낸 다음 그 때까지 과정 반복
+    
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # 먼저 단어 총 개수 알아내기
+    res_num = soup.find('div', class_ = "results-info")
+    totalnum = int(res_num['data-mw-num-results-total'])
+    results = soup.find_all('div', class_='mw-search-result-heading')
+
+    search_results = []
+
+    for div in results:
+        title_tag = div.find('a', attrs={'title': True})
+        if title_tag:
+            title = title_tag['title']
+            title = title.replace(' ', '_')
+            search_results.append(title)
+    
+    # 2번째 검색부터 반복 
+    while(nownum < totalnum) :
+        nownum = str(nownum) #str
+        search_url = "https://ko.wikipedia.org/w/index.php?search=" + search_query + "&title=특수:검색&profile=default&fulltext=1&ns0=1&offset=" + nownum + "&limit=2000"
+        response = requests.get(search_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        results = soup.find_all('div', class_='mw-search-result-heading')
+
+        for div in results:
+            title_tag = div.find('a', attrs={'title': True})
+            if title_tag:
+                title = title_tag['title']
+                search_results.append(title)
+
+        nownum = int(nownum) #int
+        nownum += 4000
+
+    return search_results
+def getWikiData(search_results):
+    wiki_url = "https://ko.wikipedia.org/wiki/" + search_results
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get(wiki_url)
+    if response.status_code == 200:
+        url = "https://ko.wikipedia.org/w/api.php?action=parse&parse&page=" + search_results + "&prop=wikitext&formatversion=2&format=json"
+        response = requests.get(url, headers=headers)
+    return response.json(), wiki_url
+
+# Tokenization of the synopsis
+def tokenizing(text):
+    text_data, url = getWikiData(text)
+    kiwi = Kiwi()
+    kiwi.prepare()
+    index = text_data['parse']['wikitext'].find("== 각주 ==")
+    if(index != -1):
+          text_data['parse']['wikitext'] = text_data['parse']['wikitext'][:index]
+    text_wikitext = text_data['parse']['wikitext']
+    result = kiwi.tokenize(text_wikitext) 
+    # return result, url, text_data, text_wikitext
+    return result, url
+
+# Extracting words and their cosine similarity values
+def make_word(result):
+    cosine_list = []
+    made_words = []
+    temp_word = ""
+    bef_tag = ""
+    bef_loc = 0
+    form_num = 0
+    cnt_dict = {}
+    cosine = {}
+
+    for form, tag, start, length in result:
+        # Nouns
+        if tag in ['NNP', 'NNG']:
+            # 명사 여러 개가 띄어쓰기 없이 나왔을 때 한 단어로 취급, 후에 바꿔도 됨(모든 명사 구분하는 걸로)
+            if bef_tag == 'NN' and bef_loc == start:
+                temp_word += form
+            # 띄어쓰기 후에 들어오는 명사
+            elif bef_tag == 'NN' and bef_loc != start:
+                if temp_word in cnt_dict:
+                    cnt_dict[temp_word] += 1
+                else:
+                    cnt_dict[temp_word] = 1
+                made_words.append(temp_word)
+                temp_word = form
+                form_num = 0
+                
+            # 관형격 조사 다음에 들어오는 명사
+            elif bef_tag == 'JKG' or bef_tag == 'XSN':
+                cosine_list.append(form)
+                temp_word += form
+                
+            elif temp_word == "":
+                temp_word = form
+
+            bef_loc = start + length
+            form_num += 1
+            bef_tag = 'NN'
+
+        # 관형격 조사일 때 ex)신의성실의 원칙
+        elif tag == 'JKG' and bef_tag == "NN":
+            cosine_list.append(form)
+            temp_word += form + " "
+            bef_tag = 'JKG'
+            form_num += 1
+
+        # ~적 (e.g., 안정적, 감각적)
+        elif tag == 'XSN' and bef_tag == "NN":
+            temp_word += form + " "
+            bef_tag = 'XSN'
+            form_num += 1
+        
+        else:
+            if bef_tag == 'NN' and len(temp_word) > 1:
+                if temp_word in cnt_dict:
+                    cnt_dict[temp_word] += 1
+                else:
+                    cnt_dict[temp_word] = 1
+                made_words.append(temp_word)
+                # for token in cosine_list:
+                #     if token in ko_model.wv.key_to_index:
+                #         cosine_sim = cosine_similarity([ko_model.wv[text_data['parse']['title']]], [ko_model.wv[token]])
+                #         if cosine_sum < cosine_sim:
+                #             cosine_sum = cosine_sim
+                #             cosine[temp_word] = cosine_sum
+
+            cosine_list = []
+            cosine_sum = 0
+            temp_word = ""
+            bef_tag = ""
+            bef_loc = 0
+            form_num = 0
+
+    return cosine, made_words, cnt_dict
+
+# 키워드와 관련된 유사도 높은 결과 단어 추출
+def getWord(text):
+    search_results = get_search_results(text)
+    page_result = {}
+    result ={}
+    cnt = 0
+    print(type(search_results))
+    print(len(search_results))
+    for search in search_results[:3]:
+        similar_word = []
+        content, url = tokenizing(search)
+        cosine, made_words,cnt_dict = make_word(content)
+        made_words_set = set(made_words)
+        final_words_list = list(made_words_set)
+        for token in final_words_list:
+            # count = 0
+            # if token in ko_model.wv.key_to_index:
+                # cosine_sim = cosine_similarity([ko_model.wv[text_data['parse']['title']]], [ko_model.wv[token]])
+                if len(token) >1:
+                    # print(f'{token}: {cosine_sim}')
+                    similar_word.append([token, cnt_dict[token],url]) 
+        for token in cosine:
+            if len(token) > 1:
+                # count = text_wikitext.count(token[0])
+                # print(f'{token}: {cosine[token]}')
+                similar_word.append([token, cnt_dict[token], url])
+        page_result[search] = similar_word
+        cnt +=1
+        if cnt%16==0:
+            time.sleep(1)
+    result[text] = page_result
+    return result
+
+if __name__ == '__main__' :
+    result = getWord("민법")
+    # print(result)
+    for key in result:
+        print(key)
+        for page in result[key]:
+             print(page)
+             for data in result[key][page]:
+                 print(data)
+    # print(result['한국사']['한국사_연표'])
+    # import pickle
+    # import gzip
+    # with gzip.open('first_crawling', 'wb') as f:
+    #     pickle.dump(result, f)
+    # print(len(result))
+# search_query = "민법"
+# start = time.time()
+# search_results = get_search_results(search_query)
+# finish = time.time()
+
+
+# print(search_results)
+# print("단어 개수 : " ,len(search_results))
+# print("걸린 시간 : ",finish - start)
+
+#한 번에 많은 단어 할 수록 시간 단축, limit 최대 5000
+#검색 한 번에 알 수 있는 최대 관련 검색어 10000개
--- lab4/server/word_dictionary/views.py
+++ lab4/server/word_dictionary/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
-from .crawling import getWord
+from .crawling_all import getWord
 
 # Create your views here.
 def index(request):
@@ -13,10 +13,18 @@ def check(request):
     #db 불러오기
     with connection.cursor() as cursor:
         # query문 실행(keyword table 가져오기)
-        cursor.execute(f"""SELECT d.word, d.usable, cast(d.created_date as char), cast(d.modified_date as char)
+        cursor.execute(f"""with tmp as
+                        (
+                        select keyword_dictionary_id, sum(frequency) as frequency
+                        from dictionary_crawling_info
+                        group by keyword_dictionary_id
+                        )
+                        -- where keyword_dictionary_id = 42 ;
+                        SELECT d.id, d.word, d.usable, cast(cast(d.created_date as date) as char), cast(cast(d.modified_date as date) as char), tmp.frequency
                         FROM mydb.keyword_dictionary d
                         join mydb.keyword k on d.keyword_id = k.id
-                        where k.keyword = '{word}';""")
+                        join tmp on d.id = tmp.keyword_dictionary_id
+                        where k.keyword = '{word}' ;""")
         # query문 결과 모두를 tuple로 저장
         rows = cursor.fetchall()
 
@@ -42,7 +50,6 @@ def keywordCollection(request):
             cursor.execute(f"""select keyword
                             from keyword ;""")
             exit_keyword = [i[0]for i in cursor.fetchall()]
-
             print(exit_keyword)
             if keyword in exit_keyword:
                 return render(request, 'main/keywordCollection.html', {"keyword": keyword, "exit_keyword": exit_keyword})
@@ -56,17 +63,21 @@ def keywordCollection(request):
                             """)
             keyword_id = (cursor.fetchone())[0]
             print(keyword_id)
-            for data in crawling_data[keyword]:
-                print(data[0])
-                cursor.execute(f"""insert into keyword_dictionary (keyword_id, word, created_date, modified_date)
-                                    values ({keyword_id}, '{data[0]}', NOW(), NOW()) ; """)
-                cursor.execute(f"""select id
-                                from keyword_dictionary
-                                where word = '{data[0]}' ;""")
-                keyword_dictionary_id = (cursor.fetchone())[0]
-                print(data[1])
-                cursor.execute(f"""insert into dictionary_crawling_info (keyword_dictionary_id, url, created_date, modified_date)
-                                values ({keyword_dictionary_id}, '{data[1]}', NOW(), NOW()) ;""")
+            for key in crawling_data:
+                for data in crawling_data[key]:
+                    # print(f'data: {crawling_data[key][data]}')
+                    for word in crawling_data[key][data]:
+                        print(word)
+                    # print(f'data[0]: {data[0]}')
+                        cursor.execute(f"""insert into keyword_dictionary (keyword_id, word, created_date, modified_date)
+                                        values ({keyword_id}, '{word[0]}', NOW(), NOW()) ; """)
+                        cursor.execute(f"""select id
+                                    from keyword_dictionary
+                                    where word = '{word[0]}' ;""")
+                        keyword_dictionary_id = (cursor.fetchone())[0]
+                    # print(f'data[1]: {data[1]}')
+                        cursor.execute(f"""insert into dictionary_crawling_info (keyword_dictionary_id, frequency, url, created_date, modified_date)
+                                    values ({keyword_dictionary_id}, {word[1]}, '{word[2]}', NOW(), NOW()) ;""")
 
     return render(request, 'main/keywordCollection.html', {"keyword": keyword})
 
@@ -75,7 +86,7 @@ def keywords(request):
     #db 불러오기
     with connection.cursor() as cursor:
         # query문 실행(keyword table 가져오기)
-        cursor.execute("""SELECT keyword, cast(created_date as char), cast(modified_date as char)
+        cursor.execute("""SELECT keyword, cast(cast(created_date as date) as char), cast(cast(modified_date as date) as char)
                         FROM keyword ;""")
         # query문 결과 모두를 tuple로 저장
         rows = cursor.fetchall()
